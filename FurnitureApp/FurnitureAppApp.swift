59be6296b7e0bcb09e90d904f679df56a744eca1
Jade Jamig <ajamig016@gmail.com>
2021-06-02 14:42:59 +0800

    Initial Commit

---
+++ FurnitureApp/FurnitureAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FurnitureAppApp.swift
+//  FurnitureApp
+//
+//  Created by Anselm Jade Jamig on 6/2/21.
+//
+
+import SwiftUI
+
+@main
+struct FurnitureAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
